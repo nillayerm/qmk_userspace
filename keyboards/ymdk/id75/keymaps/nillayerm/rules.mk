b98f0071c2bf592abf40ec0256e2d159dba86125
salmiron50 <smnam123@naver.com>
2025-01-31 10:04:50 +0900

    add keymaps

---
+++ keyboards/ymdk/id75/keymaps/nillayerm/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+# Change yes to no to disable
+
+VIA_ENABLE = yes			# Enable VIA
+LTO_ENABLE = yes 			# reduce firmware size
+KEY_LOCK_ENABLE = yes		# Enable KC_LOCK support (QK_LOCK)
